5d2dd50163a391d8b127b16c7219f66f07330e0d
unknown <miguelbadanycerne.@poli.ufrj.br>
2023-11-03 12:03:42 -0300

    Included the coverage folder and testing for files %

--- sum.js
+++ sum.js
@@ -1,5 +1,9 @@
 function sum(number1, number2) {
     return number1 + number2;
 }
+function helpmeplease(){
+    console.log("Testing the % test functionality");
+    console.log("These lines are never going to be executed");
+}
 
 module.exports = sum;
